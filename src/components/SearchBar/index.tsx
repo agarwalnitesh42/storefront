bdc512b12e2d7007097284f1287c598ecf148eea
Nitesh Agarwal <niteshagarwal@Nitesh-Agarwals-MacBook-Pro.local>
2024-11-22 14:37:37 +0530

    SearchBar Fixes

--- src/components/SearchBar/index.tsx
+++ src/components/SearchBar/index.tsx
@@ -7,11 +7,12 @@ export interface ISearch {
 const SearchBar: React.FC<ISearch> = ({onSearchClick}: ISearch) => {
 
   return (
-    <div className="flex items-center bg-white p-4 shadow rounded-lg">
+    <div className="flex items-center bg-white p-2 shadow rounded-lg">
       <input
         type="text"
         placeholder="Search Properties or Brokers"
         className="flex-1 p-2 border-none focus:ring-0 text-gray-700"
+        onFocus={onSearchClick}
       />
       <button
         onClick={onSearchClick}
